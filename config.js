6be49f44d49d9fc3df352f77ad7d4552db36edd8
Abinav Seelan <abinav.n.seelan@gmail.com>
2017-08-01 13:31:18 +0530

    Add webpack config

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
+const path = require('path');
+
 module.exports = {
   jwtSecret: 'swiggy',
   port: 1337,
+  db: 'mongodb://localhost:27017/restobook',
+  clientPath: path.join(__dirname, '/client/build'),
 };
--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
 const config = require('../config');
 const userController = require('./controllers/user');
 const postController = require('./controllers/posts');
@@ -6,6 +9,9 @@ const commentController = require('./controllers/comments');
 const replyController = require('./controllers/replies');
 
 const app = express();
+
+app.use(bodyParser.json());
+mongoose.connect(config.db);
 
 app.get('/api/posts', postController.getPosts);
 
@@ -24,6 +30,20 @@ app.post('/api/comments/:comment_id/replies', replyController.createReply);
 app.post('/api/signup', userController.signUpUser);
 
 app.post('/api/login', userController.loginUser);
+
+app.use('/', express.static(config.clientPath));
+app.get('/user/*', (request, response) => {
+  response.header('Content-type', 'text/html');
+  response.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+});
+app.get('/post/*', (request, response) => {
+  response.header('Content-type', 'text/html');
+  response.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+});
+app.get('*', (request, response) => {
+  response.header('Content-type', 'text/html');
+  response.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+});
 
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`); // eslint-disable-line no-console
